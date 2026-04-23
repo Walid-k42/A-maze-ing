5afd180306cae7bb03269d13ce2171a30ecdeec7
Walid Khazzar <khazzar.w@gmail.com>
2026-04-23 18:17:35 +0200

    start of the parsing part

---
+++ config_parser.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+from pydantic import BaseModel, Field, model_validator, field_validator
+from pydantic import ValidationError
+from enum import Enum
+from typing import Any, Annotated
+
+
+PositiveInt = Annotated[int, Field(ge=0, le=100)]
+
+
+class MazeConfig(BaseModel):
+
+    width: int = Field(ge=5, le=100)
+    height: int = Field(ge=5, le=100)
+    entry: tuple[PositiveInt, PositiveInt]
+    exit: tuple[PositiveInt, PositiveInt]
+    perfect: bool = Field(default=False)
+
+    @field_validator('entry', 'exit', mode='before')
+    @classmethod
+    def parse_string_to_tuple(cls, value: Any) -> Any:
+        if isinstance(value, tuple):
+            return value
+
+        if isinstance(value, str):
+            parts = value.split(",")
+
+            if len(parts) == 2:
+                return (int(parts[0].strip()), int(parts[1].strip()))
+
+        return value
+
+    @model_validator(mode="after")
+    def validate_maze_configs(self) -> 'MazeConfig':
+
+        if self.entry[1] > self.height or self.entry[0] > self.width:
+            raise ValueError(f"Entry coordinates {self.entry} can't be "
+                             f"outside the maze {self.height}x{self.width}")
+
+        if self.exit[1] > self.height or self.exit[0] > self.width:
+            raise ValueError(f"Exit coordinates {self.exit} can't be "
+                             f"outside the maze ({self.height}x{self.width})")
+
+        if self.exit == self.entry:
+            raise ValueError(f"The entry {self.entry} and the exit "
+                             f"{self.exit} can't be at the same location")
+
+        return self
+
+
+def read_and_split(filename: str) -> dict[str, Any]:
+
+    configs: dict = {}
+
+    try:
+        with open(filename, "r") as datas:
+            for line in datas:
+                line = line.strip()
+                if line and '=' in line:
+                    key, value = line.split("=", 1)
+                    key = key.strip().lower()
+                    value = value.strip()
+
+                    configs[key] = value
+
+    except FileNotFoundError:
+        print(f"Error: {filename} not found")
+
+    return configs
+
+
+def main() -> None:
+
+    filename = "config.txt"
+    valid_data = read_and_split(filename)
+
+    try:
+        config = MazeConfig(**valid_data)
+        print(f"test: {config}")
+
+    except ValidationError as e:
+        clean_msg = e.errors()[0]['msg'].replace("Value error, ", "")
+        print(f"Error: {clean_msg}")
+
+
+if __name__ == "__main__":
+    main()
